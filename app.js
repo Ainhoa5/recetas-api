2b70f7ccb9d77c02f50e54337a05f9810b489bff
carlosaldea3 <carlosaldea33@gmail.com>
2024-03-19 13:26:22 +0000

    No desplega
    Funcionable de manera local pero a la hora de desplegarla a heroku no va

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const setupMiddleware = require('./middleware/middleware');
-const { handle404, handleErrors } = require('./middleware/errorHandling');
 
 // Configura middlewares comunes
 setupMiddleware(app);
@@ -13,9 +15,5 @@ const recipe_routes = require("./api/routes/recipe");
 
 //Rutas base
 app.use("/api", recipe_routes);
-
-// Manejo de errores
-app.use(handle404);
-app.use(handleErrors);
 
 module.exports = app;
