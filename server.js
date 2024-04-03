a447fa0ff638d88c03c549202d730312248ca95c
carlosaldea3 <carlosaldea33@gmail.com>
2024-04-03 16:28:13 +0100

    back terminadoo v8

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 require("dotenv").config();
-const https = require("https");
-const fs = require("fs");
+const express = require("express");
 const app = require("./app");
 const connectDatabase = require("./config/database");
 const port = process.env.PORT || 3977;
 
-// Lea los archivos del certificado y la clave
-const path = require("path");
-const privateKey = fs.readFileSync(path.join(__dirname, "cert", "key.pem"));
-const certificate = fs.readFileSync(path.join(__dirname, "cert", "cert.pem"));
-
-const credentials = { key: privateKey, cert: certificate };
-
 connectDatabase().then(() => {
-  const httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(port, () => {
+  app.listen(port, () => {
     console.log(
-      `Servidor del API Rest funcionando en https://localhost:${port}`
+      `Servidor del API Rest funcionando en http://localhost:${port}`
     );
   });
 });
