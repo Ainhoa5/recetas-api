608b6f7c4da1815013bbef67613db3f4398158b7
carlosaldea3 <carlosaldea33@gmail.com>
2024-04-03 09:29:38 +0100

    Certificado SSL + dotenv
    Funciona el certificado ssl y además dotenv no funciona, se puede usar un comando propio de node para cargar las  variables de entorno y heroku las pilla sin problema

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-require('dotenv').config(); // Si decides usar variables de entorno
+require("dotenv").config();
+const https = require("https");
+const fs = require("fs");
 const app = require("./app");
 const connectDatabase = require("./config/database");
 const port = process.env.PORT || 3000;
 
+// Lea los archivos del certificado y la clave
+const path = require("path");
+const privateKey = fs.readFileSync(path.join(__dirname, "cert", "key.pem"));
+const certificate = fs.readFileSync(path.join(__dirname, "cert", "cert.pem"));
+
+const credentials = { key: privateKey, cert: certificate };
+
 connectDatabase().then(() => {
-  app.listen(port, () => {
-    console.log(`Servidor del API Rest funcionando en http://localhost:${port}`);
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(port, () => {
+    console.log(
+      `Servidor del API Rest funcionando en https://localhost:${port}`
+    );
   });
 });
-
